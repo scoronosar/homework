08740a16c78910536df40706209287985009af05
scoronosar <s.scoronosar.tj@gmail.com>
2023-10-03 17:15:08 +0500

    hw 2.2

---
+++ hw 2.2/script.js
@@ -0,0 +1,41 @@
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+let firstIphone = 2007;
+alert(firstIphone);
+let javascriptInventor = "Brendan Eich"
+alert(javascriptInventor);
+let firstNumber = 10;
+let secondNumber = 2;
+alert(firstNumber+secondNumber);
+alert(firstNumber-secondNumber);
+alert(firstNumber*secondNumber);
+alert(firstNumber/secondNumber);
+let result = 2**5;
+alert(result);
+let A = 9;
+let B = 2;
+alert(A%B);
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+let age = prompt("Сколько вам лет?")
+alert(age);
+const user = {
+    name: "John",
+    age: 18,
+    isAdmin: 'true'
+};
+user.city_of_residence = "true";
+user.age = 10;
+delete user.city_of_residence;
+let info = prompt("Какую информацию хотите узнать о пользователе?");
+alert(user[info]);
+let Name = prompt("what's your name?");
+alert("Привет, " + Name + '!');
